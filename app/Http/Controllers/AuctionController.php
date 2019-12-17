b6176d45885bc11a92cf78d517d6962cd378c1e6
Saudear <final.fantasyxii@outlook.com>
2019-12-17 16:15:47 +0700

    update code

--- app/Http/Controllers/AuctionController.php
+++ app/Http/Controllers/AuctionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DB;
+use PDF;
 use App\datacar;
 
 class AuctionController extends Controller
@@ -13,10 +14,36 @@ class AuctionController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-      $data = datacar::all();
-      return view('Auction.Home');
+
+      if ($request->get('searchDatacar') != Null) {
+
+        $idSearch = $request->get('searchDatacar');
+        $data = datacar::where('IDCard_car',$idSearch)
+                      ->orderBy('datacars.IDCard_car', 'ASC')->limit(1)
+                      ->get();
+        $Countdata = count($data);
+      }else {
+        $data = DB::table('datacars')
+        ->orderBy('datacars.IDCard_car', 'ASC')->limit(1)
+        ->get();
+
+        $Countdata = count($data);
+      }
+      return view('Auction.Home',compact('data','Countdata'));
+    }
+
+    public function Searchindex(Request $request,$id)
+    {
+
+      $data = datacar::where('id',$id)
+                    ->orderBy('datacars.IDCard_car', 'ASC')->limit(1)
+                    ->get();
+      $Countdata = count($data);
+      // dd($Countdata);
+
+      return view('Auction.Home',compact('data','Countdata'));
     }
 
     /**
@@ -71,7 +98,25 @@ class AuctionController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+      // dd($request->ClosePriceShow);
+        $data = datacar::where('id',$id)->first();
+          if ($request->ClosePriceShow != Null) {
+            $data->CloseBit_car = str_replace (",","",$request->get('ClosePriceShow'));
+          }
+            $data->OpenBit_car = str_replace (",","",$request->get('OpenPriceShow'));
+        $data->update();
+
+      $idSearch = $id + 1;
+
+      // dd($data->CloseBit_car != 0);
+      return redirect()->Route('SearchAuction',$idSearch)->with('success','อัพเดทข้อมูลเรียบร้อย');
+      //
+      // if ($request->get('ClosePriceShow') != Null or $data->CloseBit_car != 0) {
+      //   return redirect()->Route('ReportAuction',$id)->with('success','อัพเดทข้อมูลเรียบร้อย');
+      // }else {
+      //   return redirect()->Route('SearchAuction',$idSearch)->with('success','อัพเดทข้อมูลเรียบร้อย');
+      // }
+
     }
 
     /**
@@ -83,5 +128,27 @@ class AuctionController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    public function ReportAuction(Request $request, $id)
+    {
+        dd($request->input( 'name' ));
+
+        $dataReport = DB::table('datacars')
+                        ->where('datacars.id',$id)->first();
+
+        dd($dataReport);
+
+        $view = \View::make('Auction.ReportAuction' ,compact('dataReport'));
+        $html = $view->render();
+        $pdf = new PDF();
+        $pdf::SetTitle('รายงานนำเสนอ');
+        $pdf::AddPage('L', 'A4');
+        $pdf::SetMargins(5, 5, 5, 0);
+        $pdf::SetFont('freeserif', '', 8, '', true);
+        $pdf::SetAutoPageBreak(TRUE, 25);
+
+        $pdf::WriteHTML($html,true,false,true,false,'');
+        $pdf::Output('report.pdf');
     }
 }
--- resources/views/Auction/Home.blade.php
+++ resources/views/Auction/Home.blade.php
@@ -12,13 +12,15 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js" type="text/javascript"></script>
+
+        </script>
         <style>
         .fakeimg {
           height: 200px;
           background: #aaa;
         }
         </style>
-
 
         <!-- Styles -->
         <style>
@@ -77,42 +79,230 @@
     <body>
         <div class="flex-center position-ref full-height">
             <div class="content">
-                <div class="title m-b-md">
-                    บริษัท ชูเกียรติรถบ้าน จำกัด
-                </div>
+                @foreach($data as $key => $row)
+                  <script>
+                    function addCommas(nStr){
+                       nStr += '';
+                       x = nStr.split('.');
+                       x1 = x[0];
+                       x2 = x.length > 1 ? '.' + x[1] : '';
+                       var rgx = /(\d+)(\d{3})/;
+                       while (rgx.test(x1)) {
+                         x1 = x1.replace(rgx, '$1' + ',' + '$2');
+                        }
+                      return x1 + x2;
+                    }
 
-                <div class="links">
-                  <div class="jumbotron text-center" style="margin-bottom:0">
-                    <h1>My First Bootstrap 4 Page</h1>
-                    <p>Resize this responsive page to see the effect!</p>
+
+                    var OpenPrice = {{$row->OpenBit_car}};
+                    var ClosePrice = {{$row->CloseBit_car}};
+
+                    if (OpenPrice < ClosePrice) {
+                      var Price = ClosePrice;
+                    }else {
+                      var Price = OpenPrice;
+                    }
+                    // console.log(Price);
+
+
+                    function CalculatePlus(){
+                        Price += 2000;
+
+                        console.log(Price);
+                        document.getElementById('ClosePrice').innerHTML  = addCommas(Price);
+                        document.getElementById('ClosePriceShow').value  = addCommas(Price);
+                    }
+
+                    function Calculatedelete(){
+                        Price -= 2000;
+
+                        console.log(Price);
+                        document.getElementById('ClosePrice').innerHTML  = addCommas(Price);
+                        document.getElementById('ClosePriceShow').value  = addCommas(Price);
+                    }
+                  </script>
+                  <div class="title m-b-md" id="DivShow">
+                    @if($row->CloseBit_car != 0)
+                      <p id="ClosePrice" style="text-align: center;font-size: 230px;color: red; background-color: yellow; font-weight: bold;" >{{number_format($row->CloseBit_car)}}</p>
+                    @else
+                      <p name="ClosePrice" id="ClosePrice" style="text-align: center;font-size: 230px;color: red; background-color: yellow; font-weight: bold;">{{number_format($row->OpenBit_car)}}</p>
+                    @endif
                   </div>
+                  <form method="get" action="{{ route('Auction') }}">
+                    <div class="input-group">
+                        <input type="search" name="searchDatacar" class="form-control">
+                        <span class="glyphicon glyphicon-trash">
+                          <button type="submit" style="width: 150px;height: 40px;" class="delete-modal btn btn-danger btn-sm">ค้นหา</button>
+                        </span>
+                    </div>
+                  </form>
+                  <form name="form1" id="form-data" method="post" action="{{ action('AuctionController@update',$row->id) }}">
+                    @csrf
+                    @method('put')
+                    @if($row->CloseBit_car != 0)
+                      <input type="hidden" name="ClosePriceShow" id="ClosePriceShow">
+                    @else
+                      <input type="hidden" name="ClosePriceShow" id="ClosePriceShow">
+                    @endif
+                    <div class="links">
+                      <div class="jumbotron text-center" style="margin-bottom:-80px;margin-top:-45px;">
+                        <input type="hidden" name="IDCard" id="IDCard" value="{{$row->IDCard_car}}">
+                        <input type="hidden" name="Brandcar" id="Brandcar" value="{{$row->Brand_car}}">
+                        <input type="hidden" name="Regiscar" id="Regiscar" value="{{$row->Regis_car}}">
+                        <input type="hidden" name="Yearcar" id="Yearcar" value="{{$row->Year_car}}">
+                        <input type="hidden" name="Versioncar" id="Versioncar" value="{{$row->Version_car}}">
+                        <input type="hidden" name="Notecar" id="Notecar" value="{{$row->Note_car}}">
+                        <h1 style="color: red; background-color: yellow; font-weight: bold; padding: 30px;">คันที่ {{$row->IDCard_car}}</h1>
+                        <h3>ป้ายทะเบียน {{$row->Regis_car}}</h3>
+                        <div class="row">
+                          <div class="col-md-6">
+                            <h2>ราคาเปิด</h2>
+                            <h2><input type="text" id="OpenPriceShow" name="OpenPriceShow" value="{{number_format($row->OpenBit_car)}}"></h2>
+                          </div>
+                          <div class="col-md-6">
+                              @if($row->CloseBit_car != 0)
+                                <h2 style="color:red;">ราคาปิด</h2>
+                                <h2><input type="text" style="color:red;" value="{{number_format($row->CloseBit_car)}}"></h2>
+                              @endif
+                          </div>
+                        </div>
+                        <p>Resize this responsive page to see the effect!</p>
+                      </div>
 
-                  <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-                    <!-- <a class="navbar-brand" href="#">Navbar</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-                      <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="collapsibleNavbar">
-                      <ul class="navbar-nav">
-                        <li class="nav-item">
-                          <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link" href="#">Link</a>
-                        </li>
-                      </ul>
-                    </div> -->
-                    <button type="button" align="center">ตกลง</button>
-                  </nav>
+                      <div class="navbar navbar-expand-sm bg-dark navbar-dark">
+                        <div class="form-inline">
+                          <div class="row">
+                            <table border="0">
+                              <tr>
+                                <td width="600px"></td>
+                                <td width="80">
+                                  <input type="text" name="Agentcar" id="Agentcar" value="" class="form-control">
+                                </td>
+                                <td width="80">
+                                  <button type="button" id="printRe" class="delete-modal btn btn-danger btn-sm" style="width: 70px;height: 40px;"/>
+                                    <span class="glyphicon glyphicon-trash"></span> พิมพ์
+                                  </button>
+                                </td>
+                                <td width="80px">
+                                  <button type="button" class="delete-modal btn btn-danger btn-sm" style="width: 70px;height: 40px;" onclick="Calculatedelete();"/>
+                                    <span class="glyphicon glyphicon-trash"></span> ลบ
+                                  </button>
+                                </td>
+                                <td width="80px">
+                                  <button type="button" class="delete-modal btn btn-danger btn-sm" style="width: 70px;height: 40px;" onclick="CalculatePlus();"/>
+                                    <span class="glyphicon glyphicon-trash"></span> เพิ่ม
+                                  </button>
+                                </td>
+                                <td width="80px">
+                                  <button type="submit" class="delete-modal btn btn-danger btn-sm" style="width: 70px;height: 40px;">
+                                    <span class="glyphicon glyphicon-trash"></span> ถัดไป
+                                  </button>
+                                </td>
+                              </tr>
+                            </table>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
 
+                  </form>
+                @endforeach
+                <!-- <button type="button" onclick='window.print();'>print</button> -->
 
-
-                </div>
+                @if($Countdata == 0)
+                  <div class="title m-b-md">
+                    บริษัท ชูเกียรติลิสซิ่ง จำกัด
+                    <!-- <img class="img-responsive" src="{{ asset('logo.jpg') }}" alt="User Image" style = "width:30%"> -->
+                    <div class="form-inline" align="center">
+                      <a href="{{ action('AuctionController@index') }}" style="width: 150px;height: 40px;" class="delete-modal btn btn-danger btn-sm">
+                        <span class="glyphicon glyphicon-trash"></span> เริ่มประมูลใหม่
+                      </a>
+                    </div>
+                  </div>
+                @endif
             </div>
         </div>
+
+        <script type="text/javascript">
+          $(document).ready(function() {
+            $('#printRe').click(function(event) {
+              var price_close = $('#ClosePriceShow').val();
+              var price_Open = $('#OpenPriceShow').val();
+              var ID_Card = $('#IDCard').val();
+              var Brand_car = $('#Brandcar').val();
+              var Regis_car = $('#Regiscar').val();
+              var Year_car = $('#Yearcar').val();
+              var Version_car = $('#Versioncar').val();
+              var Note_car = $('#Notecar').val();
+              var Agent_car = $('#Agentcar').val();
+
+              // var divToPrint = document.getElementById('container'); // เลือก div id ที่เราต้องการพิมพ์
+             	var html =  '<html>'+ //
+             				'<head>'+
+             					'<link href="css/print.css" rel="stylesheet" type="text/css">'+
+             				'</head>'+
+             					'<body onload="window.print(); window.close();">' +
+                      '<hr />' +
+                      '<table border="2" align="center">' +
+                        '<tr><td>'+
+                          '<table border="0" align="center" width="400px">' +
+                          '<tr align="center"><td colspan="4" style="line-height:200%;">บริษัท ชูเกียรติลิสซิ่ง จำกัด</td></tr>' +
+                          '<tr align="center"><td colspan="4" style="line-height:200%;">17/8 ม.4 ต.รูสะมิแล อ.เมือง จ.ปัตตานี 94000</td></tr>' +
+                          '<tr align="left"><td colspan="4"><hr /></td></tr>' +
+                          '<tr align="left"><td colspan="4"><b>ข้อมูลรถ</b></td></tr>' +
+                          '<tr align="left"><td width="50px"></td><td width="100px">คันที่ :</td><td>' + ID_Card +'</td></tr>' +
+                          '<tr align="left"><td width="50px"></td><td width="100px">ยี่ห้อ :</td><td>' + Brand_car +'</td></tr>' +
+                          '<tr align="left"><td width="50px"></td><td width="100px">ป้ายทะเบียน :</td><td>' + Regis_car +'</td></tr>' +
+                          '<tr align="left"><td width="50px"></td><td width="100px">ปีรถ :</td><td>' + Year_car +'</td></tr>' +
+                          '<tr align="left"><td width="50px"></td><td width="100px">รายละเอียด :</td><td colspan="4">' + Version_car +'</td></tr>' +
+                          '<tr align="left"><td width="50px"></td><td width="100px">หมายเหตุ :</td><td colspan="4">' + Note_car +'</td></tr>' +
+                          '<tr align="left"><td colspan="4"><hr /></td></tr>' +
+                          '<tr align="left"><td colspan="4"><b>รายละเอียดการประมูล</b></td></tr>' +
+                          '<tr><td width="50px"></td><td width="150px">หมายเลขผู้ประมูล :</td><td align="center">หมายเลขที่ ' + Agent_car +'</td><td width="50px"></td></tr>' +
+                          '<tr><td width="50px"></td><td width="150px">ราคาเริ่มต้น :</td><td align="right">' + price_Open +' บาท</td><td width="50px"></td></tr>' +
+                          '<tr><td width="50px"></td><td width="150px">ราคาชนะประมูล :</td><td align="right">' + price_close +' บาท</td><td width="50px"></td></tr>' +
+                          '<tr><td colspan="4"></td></tr>' +
+                          '<tr><td width="50px"></td><td width="100px" align="right">ลงชื่อ :</td><td>....................................</td></tr>' +
+                          '<tr><td width="50px"></td><td width="100px" align="right"></td><td>(..................................)</td></tr>' +
+                          '</table>' +
+                        '</td></tr>' +
+                      '</table>' +
+                      '<hr />' +
+                      '<table border="2" align="center">' +
+                        '<tr><td>'+
+                          '<table border="0" align="center" width="400px">' +
+                          '<tr align="center"><td colspan="4" style="line-height:200%;">บริษัท ชูเกียรติลิสซิ่ง จำกัด</td></tr>' +
+                          '<tr align="center"><td colspan="4" style="line-height:200%;">17/8 ม.4 ต.รูสะมิแล อ.เมือง จ.ปัตตานี 94000</td></tr>' +
+                          '<tr align="left"><td colspan="4"><hr /></td></tr>' +
+                          '<tr align="left"><td colspan="4"><b>ข้อมูลรถ</b></td></tr>' +
+                          '<tr align="left"><td width="50px"></td><td width="100px">คันที่ :</td><td>' + ID_Card +'</td></tr>' +
+                          '<tr align="left"><td width="50px"></td><td width="100px">ยี่ห้อ :</td><td>' + Brand_car +'</td></tr>' +
+                          '<tr align="left"><td width="50px"></td><td width="100px">ป้ายทะเบียน :</td><td>' + Regis_car +'</td></tr>' +
+                          '<tr align="left"><td width="50px"></td><td width="100px">ปีรถ :</td><td>' + Year_car +'</td></tr>' +
+                          '<tr align="left"><td width="50px"></td><td width="100px">รายละเอียด :</td><td colspan="4">' + Version_car +'</td></tr>' +
+                          '<tr align="left"><td width="50px"></td><td width="100px">หมายเหตุ :</td><td colspan="4">' + Note_car +'</td></tr>' +
+                          '<tr align="left"><td colspan="4"><hr /></td></tr>' +
+                          '<tr align="left"><td colspan="4"><b>รายละเอียดการประมูล</b></td></tr>' +
+                          '<tr><td width="50px"></td><td width="150px">หมายเลขผู้ประมูล :</td><td align="center">หมายเลขที่ ' + Agent_car +'</td><td width="50px"></td></tr>' +
+                          '<tr><td width="50px"></td><td width="150px">ราคาเริ่มต้น :</td><td align="right">' + price_Open +' บาท</td><td width="50px"></td></tr>' +
+                          '<tr><td width="50px"></td><td width="150px">ราคาชนะประมูล :</td><td align="right">' + price_close +' บาท</td><td width="50px"></td></tr>' +
+                          '<tr><td colspan="4"></td></tr>' +
+                          '<tr><td width="50px"></td><td width="100px" align="right">ลงชื่อ :</td><td>....................................</td></tr>' +
+                          '<tr><td width="50px"></td><td width="100px" align="right"></td><td>(..................................)</td></tr>' +
+                          '</table>' +
+                        '</td></tr>' +
+                      '</table>' +
+                      '</body>'+
+             				'</html>';
+
+             	var popupWin = window.open();
+             	popupWin.document.open();
+             	popupWin.document.write(html); //โหลด print.css ให้ทำงานก่อนสั่งพิมพ์
+             	popupWin.document.close();
+              event.preventDefault();
+            });
+          });
+        </script>
     </body>
 
 </html>
